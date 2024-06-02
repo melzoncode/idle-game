c861b299a9a1b466c9d9a687f7bdd7ab017de067
melzoncode <amiludeen@icloud.com>
2024-06-02 08:25:24 +0700

    Create reportWebVitals.js

---
+++ src/reportWebVitals.js
@@ -0,0 +1,14 @@
+// src/reportWebVitals.js
+
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
